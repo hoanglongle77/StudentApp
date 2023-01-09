05215fc355c1312e2782425561ab062645d3b765
hoanglongle77 <longlnhgcd201639@fpt.edu.vn>
2023-01-09 16:03:59 +0700

    Add project files.

---
+++ StudentApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StudentApp;
+
+Student student1 = new Student("Long", 21, 3.5);
+Student student2 = new Student("Nhat", 22, 10);
+
+Console.WriteLine(student1.DisplayInformation());
+Console.WriteLine(student2.ToString());
+Console.ReadKey();
---
+++ StudentApp/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        private int age;
+        private double gpa;
+
+        public int Age { get => age; set => age=value; }
+        public double Gpa { set { if (value > 0 && value <=4) gpa=value; } get { return gpa; } }
+
+        public Student() { }
+        public Student(string name, int age, double gpa)
+        {
+            Name=name;
+            this.age=age;
+            this.gpa=gpa;
+        }
+
+        public string DisplayInformation()
+        {
+            return $"Student has Name = {Name}, Age = {age}, GPA = {gpa}";
+        }
+
+        public override string? ToString()
+        {
+            return $"Name = {Name}, Age = {age}, GPA = {gpa}";
+        }
+    }
+}
